0356cd5b9c33a7579466ec6116d2b62626366cf5
Jianfeng Song <31595152+Jianfengusa@users.noreply.github.com>
2022-11-15 07:02:45 -0800

    update

---
+++ sparsity/scripts/my_test.sh
@@ -0,0 +1,4 @@
+
+ timeloop-mapper ../arch/my_arch.yaml ../components/*.yaml ../sparse_opt/my_eyeriss_like.yaml ../constraints/my_eyeriss_like_arch_constraints.yaml ../mapper/mapper.yaml ../prob/layer3.yaml -o ../output/
+
+
